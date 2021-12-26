3ac3d1951768fe0a34e39e0e0208a42243e08d7d
vilvadot <hi@vilva.io>
2021-12-26 02:19:11 +0100

    Fiat lux

--- generator.js
+++ generator.js
@@ -1,82 +1,59 @@
 import { SVG } from "https://cdn.skypack.dev/@svgdotjs/svg.js";
 import {
-  random,
-  pointsInPath,
-} from "https://cdn.skypack.dev/@georgedoescode/generative-utils@1.0.0";
-import { findPointInCircle, half, calculateCurve, roll } from "./utils.js";
+  generatePalette,
+  generateFaviconFromSVG,
+  updateUrl,
+  getParamsFromUrl,
+} from "./utils.js";
 
-const WIDTH = 300;
-const HEIGHT = 300;
-const COLOR = "black";
-const THICKNESS = 0.3;
+const SIZE = getParamsFromUrl().size || 5;
+const HEIGHT = 50;
+const WIDTH = HEIGHT * SIZE;
 
-const RADII = parseInt(random(10, 45));
-const WEB_TENSION = () => random(0.3, 1);
-const ANCHOR_QUANTITY = random(10, 30);
-const CENTER = {
-  x: half(WIDTH) + random(-50, 50),
-  y: half(HEIGHT) + random(-50, 50),
-};
-
-const draw = SVG()
+const svg = SVG()
   .addTo("#sketch")
   .size(WIDTH, HEIGHT)
   .viewbox(0, 0, WIDTH, HEIGHT);
 
-// Draw axes radiating starting at 9 o'clock going clockwise
-const axes = [];
-const angleIncrement = 360 / RADII;
-for (let angle = 0; angle <= 360; angle += angleIncrement * random(0.5, 1.5)) {
-  let AXEL_LENGTH = random(60, 150);
-  const end = findPointInCircle(CENTER.x, CENTER.y, AXEL_LENGTH, angle);
+const drawPalette = (palette) => {
+  palette.forEach((color, index) => {
+    const size = WIDTH / SIZE;
 
-  const line = draw
-    .line(CENTER.x, CENTER.y, end.x, end.y)
-    .stroke({ color: COLOR, width: THICKNESS, linecap: "round" });
+    svg
+      .rect(size, size)
+      .x(size * index)
+      .fill(color);
+  });
 
-  // Only 25% of axes are supporting the web to the environment. Draw them on top
-  const isAnchoringWeb = roll(0.25);
-  if (isAnchoringWeb) {
-    const supportEnd = findPointInCircle(CENTER.x, CENTER.y, 500, angle);
-    draw
-      .line(CENTER.x, CENTER.y, supportEnd.x, supportEnd.y)
-      .stroke({ color: COLOR, width: THICKNESS, linecap: "round" });
-  }
+  document.querySelector("#colors").textContent = palette.toString();
+};
 
-  axes.push(line);
+const removeOldPalette = () => {
+  document.querySelectorAll("rect").forEach(square => square.remove())
 }
 
-// Close web by repeating first axel as last. Curiosly I have seen my spider does not always do this
-axes.push(axes[0]);
+const obtainPalette = (ignoreUrl) => {
+  const paletteParams = !ignoreUrl &&
+    getParamsFromUrl().palette && getParamsFromUrl().palette.split(",");
+  const palette = paletteParams || generatePalette(SIZE);
 
-// Split each axel into N anchors to stick web to them and build concentric circles
-const anchors = [];
-axes.forEach((line) => {
-  // Add endpoint so that web closes on the edges
-  const end = {
-    x: line.node.getAttribute("x2"),
-    y: line.node.getAttribute("y2"),
-  };
-  const points = pointsInPath(line.node, ANCHOR_QUANTITY).map(({ x, y }) => ({
-    x,
-    y,
-  }));
-  anchors.push([...points, end]);
+  return palette
+}
+
+const generate = (ignoreUrl) => {
+  const palette = obtainPalette(ignoreUrl)
+
+  const favicon = SVG().size(10, 10).viewbox(0, 0, 10, 10);
+  favicon.rect(10, 10).fill(palette[0]);
+
+  removeOldPalette()
+  drawPalette(palette);
+  updateUrl({ size: SIZE, palette: palette.toString()});
+  generateFaviconFromSVG(favicon.node);
+};
+
+document.querySelector("#generate").addEventListener("click", () => {
+  generate(true);
 });
-// Connect anchor points of each adjacent web strand
-anchors.forEach((points, axelIndex) => {
-  points.forEach((start, pointIndex) => {
-    const adyacentAnchor = anchors[axelIndex + 1];
 
-    if (!adyacentAnchor) return;
-
-    const end = adyacentAnchor[pointIndex];
-    const tension = pointIndex * WEB_TENSION();
-    const curve = calculateCurve(start, end, tension);
-
-    // Some segments are missing to add variety
-    const isMissing = roll(0.15);
-    const color = isMissing ? "transparent" : COLOR;
-    draw.path(curve).stroke({ color: color, width: THICKNESS }).fill("none");
-  });
-});
+generate();
--- utils.js
+++ utils.js
@@ -1,37 +1,196 @@
-export const half = (value) => value / 2;
 
-export const roll = (probability = .5) => {
-  const diceRoll = Math.random();
-  if (diceRoll <= probability) return true;
-  return false;
+export const getParamsFromUrl = () => {
+  const params = decodeURI(window.location.search.replace("?", ""))
+  return params && JSON.parse(decodeURI(params))
+}
+
+export const cleanUrl = () => {
+  history.pushState({}, "", `/`);
+}
+
+export const updateUrl = (params) => {
+  history.pushState({}, "", `?${JSON.stringify(params)}`);
+}
+
+export const random = (minimum, maximum) => {
+  return Math.floor(Math.random() * (maximum - minimum + 1) + minimum);
 };
 
-// https://stackoverflow.com/questions/32921322/=svg-marker-can-i-set-length-and-angle
-export const findPointInCircle = (x, y, distance = 100, angleInDegrees) => {
-  var radians = (angleInDegrees * Math.PI) / 180;
-
-  return {
-    x: Math.cos(radians) * distance + x,
-    y: Math.sin(radians) * distance + y,
-  };
+export const randomFrom = (array) => {
+  const index = random(0, array.length);
+  return array[index];
 };
 
-const midPointBetween = (pointA, pointB) => {
-  return {
-    x: (pointB.x + pointA.x) * 0.5,
-    y: (pointB.y + pointA.y) * 0.5,
-  };
+const getUniqueRandomColor = (palette) => {
+  const color = randomFrom(COLORS);
+  if (palette.includes(color)) return getUniqueRandomColor(palette);
+  return color;
 };
 
-// https://stackoverflow.com/questions/49274176/how-to-create-a-curved-svg-path-between-two-points
-export const calculateCurve = (pointA, pointB, offset = 30) => {
-  const midpoint = midPointBetween(pointA, pointB);
-  var theta = Math.atan2(pointB.y - pointA.y, pointB.x - pointA.x) - Math.PI / 2;
-
-  var tensor = {
-    x: midpoint.x - offset * Math.cos(theta),
-    y: midpoint.y - offset * Math.sin(theta),
-  };
-
-  return `M${pointA.x} ${pointA.y} Q${tensor.x} ${tensor.y} ${pointB.x} ${pointB.y}`;
+export const generatePalette = (size) => {
+  const palette = [];
+  for (let i = 0; i < size; i++) {
+    const color = getUniqueRandomColor(palette);
+    palette.push(color);
+  }
+  return palette;
 };
+
+export const generateFaviconFromSVG = (svg) => {
+  var link = document.createElement("link")
+  link.rel = "shortcut icon";
+  link.href = `data:image/svg+xml;utf8,${svg.outerHTML}`;
+
+  document.getElementsByTagName("head")[0].appendChild(link);
+};
+
+export const COLORS = [
+  "AliceBlue",
+  "AntiqueWhite",
+  "Aqua",
+  "Aquamarine",
+  "Azure",
+  "Beige",
+  "Bisque",
+  "Black",
+  "BlanchedAlmond",
+  "Blue",
+  "BlueViolet",
+  "Brown",
+  "BurlyWood",
+  "CadetBlue",
+  "Chartreuse",
+  "Chocolate",
+  "Coral",
+  "CornflowerBlue",
+  "Cornsilk",
+  "Crimson",
+  "Cyan",
+  "DarkBlue",
+  "DarkCyan",
+  "DarkGoldenRod",
+  "DarkGray",
+  "DarkGrey",
+  "DarkGreen",
+  "DarkKhaki",
+  "DarkMagenta",
+  "DarkOliveGreen",
+  "DarkOrange",
+  "DarkOrchid",
+  "DarkRed",
+  "DarkSalmon",
+  "DarkSeaGreen",
+  "DarkSlateBlue",
+  "DarkSlateGray",
+  "DarkSlateGrey",
+  "DarkTurquoise",
+  "DarkViolet",
+  "DeepPink",
+  "DeepSkyBlue",
+  "DimGray",
+  "DimGrey",
+  "DodgerBlue",
+  "FireBrick",
+  "FloralWhite",
+  "ForestGreen",
+  "Fuchsia",
+  "Gainsboro",
+  "GhostWhite",
+  "Gold",
+  "GoldenRod",
+  "Gray",
+  "Grey",
+  "Green",
+  "GreenYellow",
+  "HoneyDew",
+  "HotPink",
+  "IndianRed",
+  "Indigo",
+  "Ivory",
+  "Khaki",
+  "Lavender",
+  "LavenderBlush",
+  "LawnGreen",
+  "LemonChiffon",
+  "LightBlue",
+  "LightCoral",
+  "LightCyan",
+  "LightGoldenRodYellow",
+  "LightGray",
+  "LightGrey",
+  "LightGreen",
+  "LightPink",
+  "LightSalmon",
+  "LightSeaGreen",
+  "LightSkyBlue",
+  "LightSlateGray",
+  "LightSlateGrey",
+  "LightSteelBlue",
+  "LightYellow",
+  "Lime",
+  "LimeGreen",
+  "Linen",
+  "Magenta",
+  "Maroon",
+  "MediumAquaMarine",
+  "MediumBlue",
+  "MediumOrchid",
+  "MediumPurple",
+  "MediumSeaGreen",
+  "MediumSlateBlue",
+  "MediumSpringGreen",
+  "MediumTurquoise",
+  "MediumVioletRed",
+  "MidnightBlue",
+  "MintCream",
+  "MistyRose",
+  "Moccasin",
+  "NavajoWhite",
+  "Navy",
+  "OldLace",
+  "Olive",
+  "OliveDrab",
+  "Orange",
+  "OrangeRed",
+  "Orchid",
+  "PaleGoldenRod",
+  "PaleGreen",
+  "PaleTurquoise",
+  "PaleVioletRed",
+  "PapayaWhip",
+  "PeachPuff",
+  "Peru",
+  "Pink",
+  "Plum",
+  "PowderBlue",
+  "Purple",
+  "RebeccaPurple",
+  "Red",
+  "RosyBrown",
+  "RoyalBlue",
+  "SaddleBrown",
+  "Salmon",
+  "SandyBrown",
+  "SeaGreen",
+  "SeaShell",
+  "Sienna",
+  "Silver",
+  "SkyBlue",
+  "SlateBlue",
+  "SlateGray",
+  "SlateGrey",
+  "Snow",
+  "SpringGreen",
+  "SteelBlue",
+  "Tan",
+  "Teal",
+  "Thistle",
+  "Tomato",
+  "Turquoise",
+  "Violet",
+  "Wheat",
+  "White",
+  "WhiteSmoke",
+  "Yellow",
+  "YellowGreen",
+];
